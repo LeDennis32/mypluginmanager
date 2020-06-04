ff125fa726301e3c4bbbdf4be070189bf954ad1d
LeDennis32 <ledennis32@gmail.com>
2020-06-04 16:01:13 +0200

    Initial commit

---
+++ src/ledennis/mypluginmanager/MyPluginManager.java
@@ -0,0 +1,9 @@
+package ledennis.mypluginmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPluginManager extends JavaPlugin {
+	
+	public static String prefix = "§8[§amyPluginManager§8] §7";
+	
+}
